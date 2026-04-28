df55b23f144b7f0eb19e7efdaeede8c6ce005968
laplace0430 <jeon.dana0128@gmail.com>
2026-04-28 16:31:42 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,34 @@
-const CACHE_NAME = "wrong-note-pwa-v1";
-const APP_SHELL = [
+const CACHE = "wrong-note-v3";
+
+const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
+  "./privacy.html",
   "./icon-192.png",
   "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
   );
-  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE)
+            .map(key => caches.delete(key))
+        )
       )
-    )
+      .then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
@@ -32,17 +36,9 @@ self.addEventListener("fetch", event => {
 
   event.respondWith(
     caches.match(event.request).then(cached => {
-      if (cached) return cached;
-
-      return fetch(event.request)
-        .then(response => {
-          const clone = response.clone();
-          caches.open(CACHE_NAME).then(cache => {
-            cache.put(event.request, clone);
-          });
-          return response;
-        })
-        .catch(() => caches.match("./index.html"));
+      return cached || fetch(event.request).then(response => {
+        return response;
+      });
     })
   );
 });
